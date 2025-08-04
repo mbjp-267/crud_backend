f22db1662543cfe25673dc76b911b2bddbbeacc2
M. Bambang J. P. <bmbmanav09@gmail.com>
2025-08-04 11:17:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-import dotenv from 'dotenv';
-dotenv.config(); // Biasanya di index.js
-
 import express from "express";
 import cors from "cors";
 import db from "./config/Database.js";
 import UserRoute from "./routes/UserRoute.js";
 import User from "./models/UserModel.js";
+import dotenv from 'dotenv';
+dotenv.config(); // Biasanya di index.js
 
 const app = express();
-
-const startServer = async () => {
-try {
-    await db.authenticate();
-    await db.sync();
-    console.log("Database connected...");
-} catch (error) {
-    console.error("Database connection error:", error);
-}
 
 const allowedOrigins = [
   "https://crud-frontend-beige.vercel.app",
@@ -32,11 +22,22 @@ app.use(cors({
             callback(new Error("Not allowed by CORS"));
         }
     },
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true
 }));
+app.options("*", cors());
+    
 app.use(express.json());
 app.use("/users", UserRoute);
 
+const startServer = async () => {
+try {
+    await db.authenticate();
+    await db.sync();
+    console.log("Database connected...");
+} catch (error) {
+    console.error("Database connection error:", error);
+}
 // Gunakan PORT dari Railway
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
