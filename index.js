d2703dfb2feed98c26da2bbf77c89d352b09bc74
M. Bambang J. P. <bmbmanav09@gmail.com>
2025-08-04 10:03:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ try {
     console.error("Database connection error:", error);
 }
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://crud-frontend-beige.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 app.use("/users", UserRoute);
 
