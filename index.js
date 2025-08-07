07e211903f40ec539d87aa1bfdce61e69ff9468c
M. Bambang J. P. <bmbmanav09@gmail.com>
2025-08-07 13:26:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(5000, () => {
-    console.log("Server running at http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 };
 
